22ec81f2d4aa24860a561a8d93c513d1b46ee661
AKV-PC\ARUN <afm447@gmail.com>
2016-10-15 14:43:49 +0530

    images and css

---
+++ index.php
@@ -0,0 +1,299 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+    <meta name="author" content="Coderthemes">
+    <link rel="shortcut icon" href="assets/images/favicon.html">
+    <title>Album</title>
+    <link rel="stylesheet" href="assets/plugins/magnific-popup/css/magnific-popup.css" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+    <script src="assets/js/modernizr.min.js"></script>
+    <script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '../../../www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-69506598-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+</head>
+
+<body>
+    <!-- Navigation Bar-->
+    <header id="topnav">
+        <div class="topbar-main">
+            <div class="container">
+                <!-- Logo container-->
+                <div class="logo">
+                    <a href="index.html" class="logo"><span>MY ALBUM</span></a>
+                </div>
+                <!-- End Logo container-->
+            </div>
+        </div>
+    </header>
+    <!-- End Navigation Bar-->
+    <div class="container" style="top:-2em">
+        <!-- Page-Title -->
+        <div class="row">
+            <div class="col-sm-12 col-sm-6 col-lg-3 col-md-4">
+                <h4 class="page-title">Gallery</h4>
+            </div>
+        </div>
+        <div class="row port m-b-20">
+            <div class="portfolioContainer">
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (1).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (1).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (2).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (2).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (3).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (3).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (4).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (4).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (5).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (5).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (6).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (6).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (7).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (7).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (8).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (8).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (9).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (9).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (10).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (10).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (11).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (11).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (12).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (12).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (13).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (13).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (14).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (14).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (15).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (15).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (16).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (16).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (19).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (19).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (17).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (17).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (18).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (18).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (20).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (20).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (21).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (21).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (22).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (22).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (23).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (23).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (24).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (24).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (25).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (25).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div>
+                <!-- <div class="col-sm-6 col-lg-3 col-md-4">
+                    <div class="gal-detail thumb">
+                        <a href="assets/images/gallery/1 (26).jpg" class="image-popup">
+                            <img src="assets/images/gallery/1 (26).jpg" class="thumb-img" alt="work-thumbnail">
+                        </a>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+        <!-- End row -->
+    </div>
+    <!-- end container -->
+    <!-- end wrapper -->
+    <!-- jQuery  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Page related js-->
+    <script type="text/javascript" src="assets/plugins/isotope/js/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/magnific-popup/js/jquery.magnific-popup.min.js"></script>
+    <!-- App core js -->
+    <script src="assets/js/jquery.core.js"></script>
+    <script src="assets/js/jquery.app.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        var $container = $('.portfolioContainer');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        $('.portfolioFilter a').click(function() {
+            $('.portfolioFilter .current').removeClass('current');
+            $(this).addClass('current');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+    });
+    $(document).ready(function() {
+        $('.image-popup').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            mainClass: 'mfp-fade',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            }
+        });
+    });
+    </script>
+</body>
+
+</html>
